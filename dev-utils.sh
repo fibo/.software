7c81b27542d360630e14568c6e5a80e1d8ef556f
fibo <casati_gianluca@yahoo.it>
2013-09-12 17:27:52 +0200

    can checkout all branches from origin

--- dev-utils.sh
+++ dev-utils.sh
@@ -1,4 +1,28 @@
 
+.software_checkout_all_branches_from_origin() {
+	.software_list_origin_branches | while read BRANCH
+		do
+			.software_checkout_origin_branch $BRANCH
+		done
+
+	git branch
+	git checkout master
+}
+
+#-------------------------------------------------------------------------------
+
+.software_checkout_origin_branch() {
+	BRANCH=$1
+
+	git checkout -b $BRANCH origin/$BRANCH
+}
+
+#-------------------------------------------------------------------------------
+
+.software_list_origin_branches() {
+	git branch -a | grep -v master | cut -d / -f3
+}
+
 #-------------------------------------------------------------------------------
 
 .software_merge_master_from_all_branches() {
